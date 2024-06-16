f0f81a4613f05554c16893c32336d75ebf409de6
Cristiano bastos <cristhianobastos@gmail.com>
2024-06-16 18:29:06 -0300

    app biscoito da sorte

--- App.js
+++ App.js
@@ -1,69 +1,98 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-import React, {Component} from "react";
-import { View, Text, StyleSheet,TextInput, Button } from "react-native";
-
-
-
-class App extends Component{
+class App extends Component {
  constructor(props){
-  super(props);
-  this.state={
-    nome: " ",
-    input: " "
+   super(props);
+   this.state={
+     textoFrase:'',
+     img: require('./src/biscoito.png')
+   };
 
-  };
+   this.quebraBiscoito = this.quebraBiscoito.bind(this)
 
- 
-  this.entrar = this.entrar.bind(this);
+   this.frases = ['A vida trará coisas boas se tiver paciência.',
+     'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+     'Não compense na ira o que lhe falta na razão.',
+     'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+     'A maior de todas as torres começa no solo.',
+     'Não há que ser forte. Há que ser flexível.',
+     'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
+     'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+     'A juventude não é uma época da vida, é um estado de espírito.',
+     'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.'
+   ];
  }
- entrar(){
-  if(this.state.input === ''){
-    alert('Digite deu nome!')
-    return;
+
+ quebraBiscoito(){
+   let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+   this.setState({
+    textoFrase: '"' + this.frases[numeroAleatorio] +'"',
+    img: require('./src/biscoitoAberto.png')
+   })
   }
- this.setState({nome: 'Bem vindo '+ this.state.input})
- }
 
- 
- 
-  render(){
-    return(
 
-      <View>
-        <TextInput style={estyle.input}
-        placeholder="Digite seu nome" textAlign="center"
-        onChangeText={(texto) => this.setState({input: texto})}
-        
-        
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={this.state.img}
+          style={styles.img}
         />
-      <Button title="Entrar" onPress={this.entrar}/>
-      <Text style={estyle.text}>{this.state.nome}</Text>
- 
+
+        <Text style={styles.textoFrase}> {this.state.textoFrase}</Text>
+        <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}> Quebrar Biscoito</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-
-
-const estyle = StyleSheet.create({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-  },
+   
+    alignItems:'center',
+    justifyContent: 'center'
 
-  input:{
-    height:45,
-    borderWidth:1,
-    borderColor: "black",
-    margin:10,
+  },
+  img:{
+    width:250,
+    height:250,
+
+  },
+ 
+  textoFrase:{
     fontSize:20,
-    padding:12,
+    color:'#dd7b22',
+    margin:30,
+    fontStyle:'italic',
+    textAlign:'center'
+  },
+  botao:{
+    width:230,
+    height:50,
+    borderWidth:2,
+    borderColor: '#dd7b22',
+    borderRadius:25
+
+  },
+  btnArea:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center'
   },
 
-  text:{
-
-    textAlign:"center",
-    fontSize: 25
+  btnTexto:{
+    fontSize:25,
+    fontWeight:'bold',
+    color:"black"
   }
 
 });
